44a74901742c72db1917af86844170a3afe64591
mortizo <mortizo@ups.edu.ec>
2022-09-29 12:46:41 -0500

    Commit Inicial

---
+++ pds-unidad02-ooms/src/main/java/p61/PdsUnidad02Ooms.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package p61;
+
+/**
+ *
+ * @author morti
+ */
+public class PdsUnidad02Ooms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
