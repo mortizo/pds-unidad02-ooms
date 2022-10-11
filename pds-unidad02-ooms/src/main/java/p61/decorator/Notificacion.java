03c01f5c6f81a3d48c6f2427859252bae85bfe0a
mortizo <mortizo@ups.edu.ec>
2022-10-11 10:51:47 -0500

    Decorator terminado

---
+++ pds-unidad02-ooms/src/main/java/p61/decorator/Notificacion.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p61.decorator;
+
+/**
+ *
+ * @author Mauricio Ortiz Ochoa
+ */
+public interface Notificacion {
+    
+    String send();
+    
+}
